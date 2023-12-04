56a418e60868d64ca06f8336aa2a3c270beef984
KenNyagz <kennyagz681@gmail.com>
2023-12-05 00:10:49 +0300

    Basemodels

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    Class BaseModel which is the super class for all other classes
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+       Class that defines all common attributes/methods for other classes
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initializes BaseModel instances"""
+        if kwargs:
+              for key, value in kwargs.items():
+                if key is not "__class__":
+                    setattr(self, key, value)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of class BaseModel"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        attrs = self.__dict__.copy()
+        attrs['__class__'] = self.__class__.__name__
+        attrs['created_at'] = self.created_at.isoformat()
+        attrs['updated_at'] = self.updated_at.isoformat()
+        return attrs
+
+
