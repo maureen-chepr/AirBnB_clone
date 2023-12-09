03b1b36b921a9f932dc2fb2755afe73012276fdc
maureen-chepr <maureenchep95@gmail.com>
2023-12-10 00:26:26 +0300

    test for empty line console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Test cases for the console"""
+import os
 import datetime
 import uuid
 import unittest
@@ -13,31 +14,19 @@ from models.base_model import BaseModel
 
 class TestHBNBCommand_prompting(unittest.TestCase):
     """Unittest of the custom command prompt(hbnb)"""
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_prompt(self, mock_stdout):
-        """Test prompt"""
-        with patch("builtins.input", return_value="./console.py") as f:
-            HBNBCommand().onecmd("./console.py")
-            self.assertEqual(HBNBCommand.prompt, "(hbnb) ")
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_emptyline(self, mock_stdout):
-        """Test emptyline"""
-        with patch("builtins.input", return_value="") as f:
-            self.assertFalse(HBNBCommand().onecmd(""))
-            self.assertEqual("", mock_stdout.getvalue().strip())
-    
-    @patch('sys.stdout', new_callable=StringIO)
+    """@patch('sys.stdout', new_callable=StringIO)
     def test_do_EOF(self, mock_stdout):
-        """Test EOF command"""
-        with patch('builtins.input', return_value='EOF') as f:
-            with self.assertRaises(SystemExit):
-                HBNBCommand().onecmd("EOF")
-            self.assertEqual(mock_stdout.getvalue(), '')
-
+        """"""
+        with patch('builtins.input', return_value='EOF'):
+            with patch('os._exit') as mock_exit, patch('sys.stdout', new=StringIO()) as f:
+                result = HBNBCommand().onecmd("EOF")
+                self.assertTrue(result)
+                self.assertEqual(f.getvalue(), '')
+                mock_exit.assert_called_once_with(0)
+"""
     @patch('sys.stdout', new_callable=StringIO)
     def test_do_quit(self, mock_stdout):
-        """Test quit command"""
+        """Test do_quit command"""
         with patch('builtins.input', return_value='quit'):
             with patch('sys.stdout', new=StringIO()) as f:
                 result = HBNBCommand().onecmd("quit")
@@ -46,7 +35,7 @@ class TestHBNBCommand_prompting(unittest.TestCase):
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_do_create(self, mock_stdout):
-        """Test create command"""
+        """Test do_create command"""
         with patch('builtins.input', return_value='create BaseModel'):
             with patch('sys.stdout', new=StringIO()) as f:
                 result = HBNBCommand().onecmd("create BaseModel")
@@ -55,186 +44,12 @@ class TestHBNBCommand_prompting(unittest.TestCase):
                 self.assertTrue(
                     exp, "{}".format(exp)
                 )
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_show(self, mock_stdout):
-        """Test show command"""
-        with patch('builtins.input', return_value='show BaseModel 45678') as f:
-            result = HBNBCommand().onecmd("show BaseModel 45678")
-            self.assertFalse(result)
-            exp_out =  mock_stdout.getvalue().strip()
-            out = ("** too many arguments **")
-            self.assertEqual(exp_out, out)  # Rem to test success inst
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_quit(self, mock_out):
-        """Test help quit command"""
-        with patch("builtins.input", return_value="help quit") as f:
-            result = HBNBCommand().onecmd("help quit")
-            self.assertFalse(result)
-            expd_outp = "Quit command to exit the program\n        \n"
-            self.assertEqual(mock_out.getvalue(), expd_outp)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_show(self, mock_outpt):
-        """Test help show command"""
-        with patch("builtins.input", return_value="help show") as f:
-            result = HBNBCommand().onecmd("help show")
-            self.assertFalse(result)
-            expct_outp = "Prints the string representation of an instance based on the class name and id\n"
-            self.assertEqual(mock_outpt.getvalue(), expct_outp)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_destroy(self, mock_output):
-        """Test help destroy"""
-        with patch('builtins.input', return_value="help destroy") as f:
-            result = HBNBCommand().onecmd("help destroy")
-            exp_output = "Deletes an instance based on the class name and id\n"
-            self.assertEqual(mock_output.getvalue(), exp_output)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_all(self, mock_output):
-        """Test help all command"""
-        with patch('builtins.input', return_value="help all") as f:
-            result = HBNBCommand().onecmd("help all")
-            exp_outpt = "Prints all string representation of all instances\n"
-            self.assertEqual(mock_output.getvalue(), exp_outpt)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_update(self, mock_output):
-        """Test update command"""
-        with patch('builtins.input', return_value="help update") as f:
-            HBNBCommand().onecmd("help update")
-            exp_output = "Updates an instance based on the class name and id\n"
-            self.assertEqual(mock_output.getvalue(), exp_output)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_all(self, mock_stdout):
-        """Test all command"""
-        with patch('builtins.input', return_value='all BaseModel 45678') as f:
-            result = HBNBCommand().onecmd("all BaseModel 45678")
-            self.assertFalse(result)
-            exp_out =  mock_stdout.getvalue().strip()
-            out = ("** no instance found **")
-            self.assertEqual(exp_out, out)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_destroy(self, mock_stdout):
-        """Test destroy command"""
-        # Assuming id does not exist
-        with patch('builtins.input', return_value='destroy') as f:
-            HBNBCommand().onecmd("destroy")
-            self.assertEqual(mock_stdout.getvalue().strip(), '** class name missing **')
-
-"""
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_prompt(self, mock_stdout):
-        with patch("builtins.input", return_value="./console.py") as f:
-            HBNBCommand().onecmd("./console.py")
-            self.assertEqual(HBNBCommand.prompt, "(hbnb) ")
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_emptyline(self, mock_stdout):
-        with patch("builtins.input", return_value="") as f:
-            self.assertFalse(HBNBCommand().onecmd(""))
-            self.assertEqual("", mock_stdout.getvalue().strip())
     
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_EOF(self, mock_stdout):
-        """"""
-        with patch('builtins.input', return_value='EOF') as f:
-            result = HBNBCommand().onecmd("EOF")
-            self.assertTrue(result)
-            self.assertEqual(mock_stdout.getvalue(), '')
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_quit(self, mock_stdout):
-        """"""
-        with patch('builtins.input', return_value='quit'):
-            result = HBNBCommand().onecmd("quit")
-            self.assertTrue(result)i
-            self.assertEqual(mock_stdout.getvalue(), '')
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_create(self, mock_stdout):
-        """"""
-        with patch('builtins.input', return_value='create BaseModel'):
-            result = HBNBCommand().onecmd("create BaseModel")
-            self.assertFalse(result)
-            exp = mock_stdout.getvalue().strip()
-            self.assertTrue(
-                    exp, "{}".format(exp)
-                    )
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_show(self, mock_stdout):
-        """"""
-        with patch('builtins.input', return_value='show BaseModel 45678'):
-            result = HBNBCommand().onecmd("show BaseModel 45678")
-            self.assertFalse(result)
-            exp_out = mock_stdout.getvalue().strip()
-            out = ("** too many arguments **")
-            self.assertEqual(exp_out, out)  # Rem to test success inst
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_quit(self, mock_out):
-        """"""
-        with patch("builtins.input", return_value="help quit"):
-            result = HBNBCommand().onecmd("help quit")
-            self.assertFalse(result)
-            expd_outp = "Quit command to exit the program\n        \n"
-            self.assertEqual(mock_out.getvalue(), expd_outp)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_show(self, mock_outpt):
-        """"""
-        with patch("builtins.input", return_value="help show"):
-            result = HBNBCommand().onecmd("help show") 
-            self.assertFalse(result)
-            expct_outp = "Prints the string representation of an instance based on the class name and id\n"
-            self.assertEqual(mock_outpt.getvalue(), expct_outp)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_destroy(self, mock_output):
-        """"""
-        with patch('builtins.input', return_value="help destroy"):
-            result = HBNBCommand().onecmd("help destroy")
-            exp_output = "Deletes an instance based on the class name and id\n"
-            self.assertEqual(mock_output.getvalue(), exp_output)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_help_all(self, mock_output):
-        """"""
-        with patch('builtins.input', return_value="help all"):
-            result = HBNBCommand().onecmd("help all")
-            exp_outpt = "Prints all string representation of all instances\n"
-            self.assertEqual(mock_output.getvalue(), exp_outpt)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_update(self, mock_output):
-        """"""
-        with patch('builtins.input', return_value="help update"):
-            HBNBCommand().onecmd("help update")
-            exp_output = "Updates an instance based on the class name and id\n"
-            self.assertEqual(mock_output.getvalue(), exp_output)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_all(self, mock_stdout):
-        """"""
-        with patch('builtins.input', return_value='all BaseModel 45678'):
-            result = HBNBCommand().onecmd("all BaseModel 45678")
-            self.assertFalse(result)
-            exp_out = mock_stdout.getvalue().strip()
-            out = ("** no instance found **")
-            self.assertEqual(exp_out, out)
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_do_destroy(self, mock_stdout):
-        """"""
-        # Assuming id does not exist
-        with patch('builtins.input', return_value='destroy'):
-            HBNBCommand().onecmd("destroy")
-            self.assertEqual(mock_stdout.getvalue().strip(), '** class name missing **')
-    """
+    def test_emptyline(self):
+        """Test empty line input"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("\n")
+            self.assertEqual('', f.getvalue())
 
 if __name__ == '__main__':
     unittest.main()
